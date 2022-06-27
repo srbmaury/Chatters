7f7dbbc08343d3f352adc7700c0208cda11fd34a
Saurabh Maurya <85755081+srbmaury@users.noreply.github.com>
2022-06-27 18:22:33 +0530

    fixed issues

--- accounts/models.py
+++ accounts/models.py
@@ -16,8 +16,8 @@ class profile(models.Model):
     def __str__(self):
         return f'{self.user.username} profile'
 
-    def save(self):
-        super().save()
+    def save(self, *args, **kwargs):
+        super(profile, self).save(*args, **kwargs)
 
         img = Image.open(self.image.path)
 
